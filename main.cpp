511fdc27f493b1d01b141aebc5207a080a89770e
Kyran Butler <kyran@butlerhq.com>
2019-03-31 14:48:09 -0600

    added rough draft of classes and main menu functionality

---
+++ main.cpp
@@ -0,0 +1,78 @@
+/** CSCI-2270 - Data Structures Project. 
+
+John Salame - 
+Kyran Butler - 108492940
+Inji Chung - 
+
+*/
+
+#include <iostream>
+#include <string>
+
+struct Contact { // actual contact
+
+	std::string name;
+	std::string phoneNumber;
+	std::string birthdate;
+	std::string socialSecurityNumber;
+	std::string creditCardNumber;
+	std::string threeDigitsOnTheBack;
+
+};
+
+struct treeNode { // used to build tree of contacts
+
+	treeNode* leftChild; // if organized by name, this name is less than the current name
+	treeNode* rightChild; // if organized by name, this name is greater than the current name
+	Contact c; // contact information located at tree pos
+
+};
+
+
+
+class Contacts {
+public:
+	void addContact();
+	void deleteContact();
+	void printContact();
+private:
+	treeNode root;
+};
+
+
+
+void m() { // main menu
+	std::cout << "--=[ Contact Book ]=--" << std::endl;
+	std::cout << "1) Print All Contacts" << std::endl;
+	std::cout << "2) Search For Contact" << std::endl;
+	std::cout << "3) Add Contact" << std::endl;
+	std::cout << "4) Edit Contact" << std::endl;
+	std::cout << "5) Delete Contact" << std::endl;
+	std::cout << "6) Close Book" << std::endl;
+}
+
+// everything that's here is super rough just to get some work done
+int main(){
+	std::string mainMenuChoice = "";
+	while (mainMenuChoice != "6") {
+		m();
+		std::cin >> mainMenuChoice;
+
+		switch (stoi(mainMenuChoice)) {
+		case 1: // User selected to print all of the contacts in their book.
+			break;
+		case 2: // User selected to search for an existing contact
+			break;
+		case 3: // User selected to add a new contact
+			break;
+		case 4: // User selected to edit an existing contact
+			break;
+		case 5: // User selected to delete an existing contact
+			break;
+		case 6: // User selected to close the book. Should put saving into here.
+			std::cout << "*closes book*" << std::endl;
+			break;
+		}
+	}
+	return 0;
+}
