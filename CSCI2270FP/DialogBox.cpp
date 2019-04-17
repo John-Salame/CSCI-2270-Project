99bd679e63a084a15a9a121163c0d9ee39fee3c6
In Ji Chung <inch0974@colorado.edu>
2019-04-17 15:33:02 -0600

    test

---
+++ CSCI2270FP/DialogBox.cpp
@@ -0,0 +1,9 @@
+#include "DialogBox.h"
+
+CDialogBoxes::CDialogBoxes():CDialog(CDialogBoxes::IDD) {
+
+}
+
+CDialogBoxes::~CDialogBoxes() {
+
+}
---
+++ CSCI2270FP/main.cpp
@@ -0,0 +1,33 @@
+/** CSCI-2270 - Data Structures Project.
+
+John Salame - 109001053
+Kyran Butler - 108492940
+Inji Chung -
+
+g++ -std=c++11 main.cpp -o runner
+*/
+
+#include <iostream>
+#include <string>
+#include <afxwin.h>
+#include "mainFrame.h"
+#include "resource.h"
+#include "Contacts.h"
+
+CMyApp happyApp;
+Contacts iCloud;
+
+BEGIN_MESSAGE_MAP(CMyFrame, CFrameWnd)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_PAINT()
+	ON_WM_CREATE()
+	ON_WM_KEYDOWN()
+END_MESSAGE_MAP()
+
+BOOL CMyApp::InitInstance() {
+	m_pMainWnd = new CMyFrame();
+	m_pMainWnd->ShowWindow(SW_SHOW);
+	m_pMainWnd->UpdateWindow();
+
+	return TRUE;
+}
